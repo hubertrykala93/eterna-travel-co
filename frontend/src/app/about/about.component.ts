f14b898fc974c945d6211cbaf783a543f43b5b34
hubertrykala93 <hubert.rykala@gmail.com>
2025-01-11 18:03:55 +0100

    styling about us

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  firstImageUrl: string = environment.mediaUrl + 'about/1085.jpg';
+  secondImageUrl: string = environment.mediaUrl + 'about/2580.jpg';
 }
